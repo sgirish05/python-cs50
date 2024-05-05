9b9f105d8999aa29ef0ccabc159087f6fa935585
sgirish05 <85393479+sgirish05@users.noreply.github.com>
2024-05-05 23:00:07 +0530

    first file

---
+++ hello.py
@@ -0,0 +1,25 @@
+def hello(to = "world"):
+    print("hello,",to)
+
+hello()
+name = input("what's your name? ")
+hello(name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#use strip to remove addional space and title to capitlize the 1st letter of first name and last name
+#name=input("what's your name? ").strip().title()
+#first, last = name.split(" ")
+
+#say hello to user
+#print(f"hello, {first}")
